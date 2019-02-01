ee3e65945713fb22eb160fcf75b7bcf09d4a6f3a
David Gasquez <davidgasquez@gmail.com>
2019-02-01 15:39:23 +0100

    Add dbt

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="budbt",
-    version="0.1.0",
+    version="0.1.1",
     author="David Gasquez",
     author_email="davidgasquez@buffer.com",
-    install_requires=["stacklogging"],
+    install_requires=["stacklogging", "dbt"],
     license="MIT license",
     entry_points={"console_scripts": ["budbt = budbt:main"]},
 )
